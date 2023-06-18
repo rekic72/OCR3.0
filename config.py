8ef34eed68f511770cf42dbe4f83da83cdce1573
rekic <201972@fhwn.ac.at>
2023-06-18 16:08:16 +0200

    code v04 connection chatgpt 180623 enable template usage in chatGPTfunction

--- config.py
+++ config.py
@@ -4,12 +4,16 @@ import cv2
 import fitz
 import numpy as np
 import pytesseract
+import spacy
 
 config = configparser.ConfigParser()
 config.read("config/config.ini")
 
 pytesseract.pytesseract.tesseract_cmd = config.get("DEFAULT", "tesseract_path")
 
+# Load the German model from spacy
+nlp = spacy.load("de_core_news_sm")
+print("Spacy model loaded")
 
 def process_file(file_path):
     if file_path.endswith('.pdf'):
@@ -30,7 +34,7 @@ def extract_text_from_pdf(filename):
             pix = page.get_pixmap(matrix=fitz.Matrix(2, 2), alpha=False)
             img = np.frombuffer(pix.samples, dtype=np.uint8).reshape(pix.height, pix.width, 3)
 
-            # Preprocessing
+            # Preprocessing / useful for handwritten text
             gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
             blurred_img = cv2.GaussianBlur(gray_img, (5, 5), 0)
             _, thresh_img = cv2.threshold(blurred_img, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
@@ -38,14 +42,26 @@ def extract_text_from_pdf(filename):
             # here we got to retrieve the german language package from pytesseract stored locally on a C:drive
             text += pytesseract.image_to_string(thresh_img, lang='deu')
 
+            # Look for the three words following "Therapieempfehlung"
+            index = text.find("Therapieempfehlung")
+            if index != -1:
+                words_after = text[index:].split()[1:5]  # Get the next three words
+                drug = " ".join(words_after)
+                print("Drug:", drug)
+
         return text
 
 
 def count_words(text):
-    words = text.lower().split()
+    # Process the text with spaCy -> NLP model
+    doc = nlp(text.lower())
+
+    # Filter out words so that only NOUNS are stored to the DB
+    words = [token.text for token in doc if token.pos_ == 'NOUN']
+    print(words)
+
+    # Count the filtered words
     return collections.Counter(words)
 
 
 word_counts = collections.Counter()
-
-
--- main.py
+++ main.py
@@ -13,66 +13,76 @@ mycursor = conn.cursor()
 
 mycursor.execute(create_table_query)
 
-openai.api_key = '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'
+openai.api_key = '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'
 
 
 class OCRApp(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.master = master
-        self.master.protocol("WM_DELETE_WINDOW", self.on_closing)  # closing protocol
-        self.grid(sticky="nsew")
+        self.master.protocol("WM_DELETE_WINDOW", self.on_closing)
+
+        # Create canvas and scrollbar
+        self.canvas = tk.Canvas(self.master)
+        self.scrollbar = tk.Scrollbar(self.master, orient="vertical", command=self.canvas.yview)
+        self.scrollable_frame = tk.Frame(self.canvas)
+        self.canvas.configure(yscrollcommand=self.scrollbar.set)
+        self.canvas.pack(side="left", fill="both", expand=True)
+        self.scrollbar.pack(side="right", fill="y")
+        self.canvas.create_window((0, 0), window=self.scrollable_frame, anchor="nw")
+        self.scrollable_frame.bind("<Configure>", lambda e: self.canvas.configure(scrollregion=self.canvas.bbox("all")))
+
         self.create_widgets()
         self.processed_files = set()  # used for storing file_paths
+
 
     def create_widgets(self):
         self.upload_button = tk.Button(self, text="Upload File", command=self.upload_file)
         self.upload_button.grid(row=0, column=0, sticky="w", padx=(10, 0), pady=(10, 0))
 
-        self.result_label = tk.Label(self, text="")
-        self.result_label.grid(row=1, column=0, sticky="w", padx=(10, 0), pady=(10, 0))
-
-        self.database_contents = tk.Text(self, wrap=tk.WORD, height=10, width=50)
-        self.database_contents.grid(row=2, column=0, sticky="nsew", padx=(10, 0), pady=(10, 0))
-
-        self.scrollbar = tk.Scrollbar(self, command=self.database_contents.yview)
-        self.scrollbar.grid(row=2, column=1, sticky="ns", pady=(10, 0))
-        self.database_contents.config(yscrollcommand=self.scrollbar.set)
-
+        # Print Database button
         self.print_db_button = tk.Button(self, text="Print database", command=self.print_database_contents)
         self.print_db_button.grid(row=3, column=0, sticky="w", padx=(10, 0), pady=(10, 0))
-
-        self.columnconfigure(0, weight=1)
-        self.rowconfigure(2, weight=1)
-
-        # ChatGPT input field
-        self.chat_input = tk.Entry(self)
-        self.chat_input.grid(row=4, column=0, sticky="ew", padx=(10, 0), pady=(10, 0))
-
-        # ChatGPT output field
-        self.chat_output = tk.Text(self, wrap=tk.WORD, height=5, width=50)
-        self.chat_output.grid(row=5, column=0, sticky="nsew", padx=(10, 0), pady=(10, 0))
-
-        # Add a scrollbar to the chat_output widget
-        self.chat_output_scrollbar = tk.Scrollbar(self, command=self.chat_output.yview)
-        self.chat_output_scrollbar.grid(row=5, column=1, sticky="ns", pady=(10, 0))
-        self.chat_output.config(yscrollcommand=self.chat_output_scrollbar.set)
 
         # ChatGPT submit button
         self.chat_button = tk.Button(self, text="Chat with GPT", command=self.chat_with_gpt)
         self.chat_button.grid(row=6, column=0, sticky="w", padx=(10, 0), pady=(10, 0))
 
-        # Make the chat_input widget stretch when the window is resized
-        self.columnconfigure(0, weight=1)
-        # Make the chat_output widget stretch vertically when the window is resized
-        self.rowconfigure(5, weight=1)
+        # result_label for feedback to the user
+        self.result_label = tk.Label(self, text="")
+        self.result_label.grid(row=1, column=0, sticky="w", padx=(10, 0), pady=(10, 0))
+
+        # database_contents
+        self.database_contents = tk.Text(self, wrap=tk.WORD, height=20, width=50)
+        self.database_contents.grid(row=2, column=0, sticky="nsew", padx=(10, 0), pady=(10, 0))
+
+        # Text widget for displaying the response from GPT
+        self.gpt_response = tk.Text(self, wrap=tk.WORD, height=10, width=50)
+        self.gpt_response.grid(row=7, column=0, sticky="nsew", padx=(10, 0), pady=(10, 0))
+
+        self.database_contents.bind("<ButtonRelease-1>", self.on_text_widget_click)
+
+    def on_text_widget_click(self, event):
+        # Clear any previous selection
+        self.database_contents.tag_remove("selected", "1.0", tk.END)
+
+        # Try to get the selected text
+        try:
+            selected_word = self.database_contents.get(tk.SEL_FIRST, tk.SEL_LAST)
+
+            # Highlight the selected text
+            if selected_word:
+                self.database_contents.tag_add("selected", tk.SEL_FIRST, tk.SEL_LAST)
+                self.database_contents.tag_configure("selected", background="gray")
+        except tk.TclError:
+            # Handle the case when no text is selected
+            pass
 
     def upload_file(self):
         file_path = filedialog.askopenfilename(filetypes=[("PDF Files", "*.pdf")])
         if file_path:
             if file_path in self.processed_files:
-                self.result_label.config(
-                    text=f"Error: The file '{os.path.basename(file_path)}' has already been uploaded and processed.")
+                print(f"Error: The file '{os.path.basename(file_path)}' has already been uploaded and processed.")
                 return
 
             try:
@@ -95,10 +105,10 @@ class OCRApp(tk.Frame):
                 file_name = os.path.basename(file_path)
 
                 self.result_label.config(
-                    text=f"OCR text extraction successful for '{file_name}'. Results saved to {output_file}."
-                )
+                    text=f"OCR text extraction successful for '{file_name}'. Results saved to {output_file}.")
+
             except Exception as e:
-                self.result_label.config(text=f"Error processing the file: {e}")
+                print(f"Error processing the file: {e}")
 
     def on_closing(self):
         mycursor.close()
@@ -108,44 +118,64 @@ class OCRApp(tk.Frame):
     def print_database_contents(self):
         mycursor.execute("SELECT * FROM pdf_text")
         rows = mycursor.fetchall()
-        db_contents = "\nDatabase Contents:\nword_id   |   Text   |   Count\n"
-        for row in rows:
-            db_contents += f"{row[0]} | {row[1]} | {row[2]}\n"
 
-        self.database_contents.delete(1.0, tk.END)
-        self.database_contents.insert(tk.END, db_contents)
+        # Clear the Text widget
+        self.database_contents.delete("1.0", tk.END)
+
+        # Add words to the Text widget
+        for row in rows:
+            word = row[1]
+            self.database_contents.insert(tk.END, f"{word}\n")
 
     def chat_with_gpt(self):
-        user_message = self.chat_input.get()
-        if user_message:
-            try:
-                response = openai.ChatCompletion.create(
-                    model="gpt-3.5-turbo",
-                    messages=[
-                        {
-                            "role": "system",
-                            "content": "You are a helpful assistant."
-                        },
-                        {
-                            "role": "user",
-                            "content": user_message
-                        }
-                    ]
-                )
-                self.chat_output.delete(1.0, tk.END) ###used for deleting former response messages###
-                self.chat_output.insert(tk.END, response['choices'][0]['message']['content'])
-            except Exception as e:
-                self.chat_output.config(text=f"Error processing the chat: {e}")
+        # Get the selected word
+        try:
+            selected_word = self.database_contents.get(tk.SEL_FIRST, tk.SEL_LAST)
+        except tk.TclError:
+            # Handle the case when no text is selected
+            selected_word = None
+
+        # Check if a word is selected
+        if not selected_word:
+            self.result_label.config(text="Please select a word from the database before chatting with GPT.")
+            return
+
+        print(f"Selected word: '{selected_word}'")
+
+        message_template = f"Tell me everything about: {selected_word}"
+
+        try:
+            response = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=[
+                    {
+                        "role": "system",
+                        "content": "You are a helpful assistant."
+                    },
+                    {
+                        "role": "user",
+                        "content": message_template
+                    }
+                ]
+            )
+
+            print(response)
+            self.gpt_response.delete("1.0", tk.END)
+            self.gpt_response.insert(tk.END, response['choices'][0]['message']['content'])
+        except Exception as e:
+            self.result_label.config(text=f"Error processing the chat: {e}")
+            print(f"Exception Type: {type(e)}")
+            print(f"Exception Args: {e.args}")
+            print(f"Exception: {e}")
 
 
 def main():
     root = tk.Tk()
-    root.geometry("500x300")
+    root.geometry("800x600")
     root.title("OCR Text Extraction")
     app = OCRApp(master=root)
-    root.columnconfigure(0, weight=1)
-    root.rowconfigure(0, weight=1)
-    app.mainloop()
+    app.pack(side="left", fill="both", expand=True)
+    root.mainloop()
 
 
 # ensure that code is executed only when script is running directly / not when imported
